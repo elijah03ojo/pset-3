65c2ccb134629ba599ab1c8d9ba9355abf1526cd
elijah03ojo <44117114+elijah03ojo@users.noreply.github.com>
2019-11-01 13:08:54 -0400

    Add files via upload

---
+++ grades.js
@@ -0,0 +1,22 @@
+const readlineSync = require("readline-sync");
+
+
+const grade = readlineSync.question("\nEnter a grade: ");
+
+if (grade >= 90 && grade <= 100)
+    console.log("You received an A");
+
+if (grade >= 80 && grade <= 89)
+    console.log("You received a B");
+
+if (grade >= 70 && grade <= 79)
+    console.log("You received a C");
+
+if (grade >= 60 && grade <= 69)
+    console.log("You received a D");
+
+if (grade >= 0 && grade <= 59)
+    console.log("You received a F");
+
+else
+  console.log("Invalid")
